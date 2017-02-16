a47889c48f1001262d4a6f5bd6737f6e30f8b295
Pete <pafischer1992@gmail.com>
2017-02-16 21:15:40 +0100

    Refactored structure, functions regarding manipulating dir content is found in the \files folder.

---
+++ files/dirContent.go
@@ -0,0 +1,50 @@
+package dirContent
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
+)
+
+// Directory content struct
+type Content struct {
+	Name string
+	Path string
+	Size int64
+	Type string
+}
+
+// Get directory Content and return
+func getDirectoryInput(dir string) []byte {
+	// Retrieve all the files in the input directory
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var listOfContent []Content
+
+	// Create a Content struct for each file and convert it to a JSON object.
+	for _, file := range files {
+		ext := filepath.Ext(file.Name())
+		name := strings.TrimSuffix(file.Name(), ext)
+		c := Content{
+			Name: name,
+			Path: dir,
+			Size: file.Size(),
+			Type: ext}
+
+		listOfContent = append(listOfContent, c)
+	}
+
+	// Marshall a JSON-encoded string of the listOfContent
+	mList, err := json.Marshal(listOfContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return mList
+
+}
--- filly.go
+++ filly.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"path/filepath"
-	"strings"
 )
 
 // Root directory global variable
@@ -29,45 +25,4 @@ func main() {
 func setRootDir(newRootDir string) {
 	//TODO: Where does sanitating user input happen?
 	ROOT_DIR = newRootDir
-}
-
-// Get directory Content and return
-func getDirectoryInput(dir string) []byte {
-	// Retrieve all the files in the input directory
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var listOfContent []Content
-
-	// Create a Content struct for each file and convert it to a JSON object.
-	for _, file := range files {
-		ext := filepath.Ext(file.Name())
-		name := strings.TrimSuffix(file.Name(), ext)
-		c := Content{
-			Name: name,
-			Path: dir,
-			Size: file.Size(),
-			Type: ext}
-
-		listOfContent = append(listOfContent, c)
-	}
-
-	// Marshall a JSON-encoded string of the listOfContent
-	mList, err := json.Marshal(listOfContent)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return mList
-
-}
-
-// Directory content struct
-type Content struct {
-	Name string
-	Path string
-	Size int64
-	Type string
 }
